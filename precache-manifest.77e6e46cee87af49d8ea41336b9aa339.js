fcd83bdd993bc5de56288957950773fb1f827c4f
PioKl <kosiu342@gmail.com>
2019-12-10 19:29:58 +0100

    Updates

---
+++ precache-manifest.77e6e46cee87af49d8ea41336b9aa339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d79a4acfe983124ff9b",
+    "url": "/ToDoList-React/static/css/main.4745a7e8.chunk.css"
+  },
+  {
+    "revision": "0d79a4acfe983124ff9b",
+    "url": "/ToDoList-React/static/js/main.b3ec046c.chunk.js"
+  },
+  {
+    "revision": "bfaa32f1797b435fc275",
+    "url": "/ToDoList-React/static/js/runtime~main.88779d92.js"
+  },
+  {
+    "revision": "2a032cb8a3c2e81f734b",
+    "url": "/ToDoList-React/static/js/2.dbdfff11.chunk.js"
+  },
+  {
+    "revision": "a4a6b9c7b7df94c11495599457161d39",
+    "url": "/ToDoList-React/index.html"
+  }
+];
